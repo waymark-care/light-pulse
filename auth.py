cc2a0aabc5ffad0fa39686e28971de1d0ee4b745
katieli-waymark <katie.li@waymarkcare.com>
2024-01-30 13:36:37 -0700

    Authentication code

---
+++ auth.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta, timezone
+from pydantic import BaseModel
+from typing import Union
+
+from passlib.context import CryptContext
+from jose import jwt
+
+from sqlalchemy.orm import Session
+
+from src import schemas, crud
+
+SECRET_KEY = "3b99bb090543a635183501a25bb3436ade347e7de4b4b33ce584c5c2a8c3f39d"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+def verify_password(plain_password, hashed_password):
+    # return pwd_context.verify(plain_password, hashed_password)
+    # TODO: remove this line when we are encrypting the password in the LH DB
+    #       note that this reflects the current behavior in Shipyard
+    return True
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db: Session, username: str):
+    user = crud.get_waymarker(db, id=username)
+    if user:
+        return schemas.Waymarker(**user.__dict__)
+
+
+async def authenticate_user(db: Session, id: str, api_key: str):
+    user = get_user(db, id)
+    if not user:
+        return False
+    if not verify_password(api_key, get_password_hash(user.apiKey)):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None]):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import Depends, FastAPI, HTTPException
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import JWTError, jwt
 
 from sqlalchemy.orm import Session
 
-from src import crud, models
+from src import crud, models, schemas
 from src.database import SessionLocal, engine
+
+from auth import (
+    Token,
+    TokenData,
+    SECRET_KEY,
+    ALGORITHM,
+    get_user,
+    authenticate_user,
+    create_access_token,
+)
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -36,3 +51,67 @@ async def read_patient(patient_id: str, db: Session = Depends(get_db)):
     if db_patient is None:
         raise HTTPException(status_code=404, detail="Patient not found")
     return db_patient
+
+
+# Authentication code
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db=Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[schemas.Waymarker, Depends(get_current_user)]
+) -> schemas.Waymarker:
+    if not current_user.active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db),
+) -> Token:
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.id}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/")
+async def read_users_me(
+    current_user: Annotated[schemas.Waymarker, 
+                            Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
+):
+    return current_user
